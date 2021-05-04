9cb6b8203fafb8e5be78845ce3ef9c02f9841f7c
aarora <aarora0301@gmail.com>
2021-05-04 22:26:56 +0530

    add source folder

---
+++ src/main/java/com/example/demo/WebSocketConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+
+import lombok.SneakyThrows;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import java.io.IOException;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfiguration implements WebSocketConfigurer {
+
+    @SneakyThrows
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new SocketHandler(), "/socket")
+                .setAllowedOrigins("*");
+    }
+
+}
